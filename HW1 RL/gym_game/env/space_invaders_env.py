58edc31b4506939eebd35e34037e64b0b40a590c
Faber <fabriziocasadei97@gmail.com>
2020-11-18 13:21:50 +0100

    release hw

--- HW1 RL/gym_game/env/space_invaders_env.py
+++ HW1 RL/gym_game/env/space_invaders_env.py
@@ -17,15 +17,16 @@ class SpaceInvadersEnv(gym.Env):
 
         # create rewards table
         self.rewards = np.zeros((self.rows, self.cols))
-        self.rewards[self.rows - 1] = - self. step_cost  # step cost of moves
+        self.rewards[self.rows - 1] = - self.step_cost  # step cost of moves
         self.shootReward = 0
+        self.hit_board_reward = 0
 
         # creation of invaders
         self.initial_number_invaders = 7
         self.createinvaders()
 
         for r, c in self.invaders:
-            self.rewards[r, c] = + 0.5
+            self.rewards[r, c] = + 2.0
 
         # initialization of the matrix of states and definition of the current state
         self.states = np.zeros((self.rows, self.cols))
@@ -39,15 +40,17 @@ class SpaceInvadersEnv(gym.Env):
             [[0, -1], [0, 1], self.shoot_property])  # shoot dictionary for the activation of the shoot func.
 
         # define the transition model P(s'|s,a)
-        self._transition_probabilities = np.array([0.1, 0.8, 0.1])
+        self.transition_probabilities = np.array([0.1, 0.8, 0.1])
 
-    def createinvaders(self, full= False):
+    def createinvaders(self, is_full= False, is_presetted = True):
         invs = []
-        if full:
+        if is_full:
             for a in range(5):
                 for b in range(7):
                     invs.append((a, b))
-        else:
+        elif (is_presetted):
+            invs= [(3, 6), (3, 0), (3, 1), (4, 1), (1, 1)]
+        else:  # random disposition of all the invaders
             draw_number = 0
             while draw_number < self.initial_number_invaders:
                 m = np.random.randint(0,5)
@@ -61,18 +64,47 @@ class SpaceInvadersEnv(gym.Env):
 
 
     def reset(self):
+        self.rewards = np.zeros((self.rows, self.cols))
+        self.rewards[self.rows - 1] = - self.step_cost  # step cost of moves
+        self.shootReward = 0
+        self.hit_board_reward = 0
+
+        # creation of invaders
+        self.createinvaders()
+
+        for r, c in self.invaders:
+            self.rewards[r, c] = + 2.0
+
+        # initialization of the matrix of states and definition of the current state
+        self.states = np.zeros((self.rows, self.cols))
+        self.init_state = (self.rows - 1, 3, self.invaders)
         self.current_state = self.init_state
+
+# start setter & getter methods
+    def getCurrentState(self):
+        return self.current_state
+
+    def setCurrentState(self, state):
+        self.current_state = state
+
+    def getRewards(self):
+        return self.rewards
+
+    def setRewards(self, rewards):
+        self.rewards = rewards
+# end setter & getter methods
 
     def termination(self):
         r, l, c = self.current_state
         return len(c) == self.goal  # killed all the invaders
 
+    # shoot action which remove the first invader in front of the space ship
     def shoot(self, row):
         x_pos = self.current_state[1]
         if row == -1:  # end of the grid
             return self.shootReward
         else:
-            if self.rewards[row, x_pos] == 0.5:
+            if self.rewards[row, x_pos] == 2.0:
                 r, c, invaders = self.current_state
                 invaders.remove((row, x_pos))
                 self.shootReward = self.rewards[row, x_pos]  # the ship has hit an invader
@@ -81,32 +113,54 @@ class SpaceInvadersEnv(gym.Env):
             else:
                 self.shoot((row - 1))
 
-    def reward(self) -> float:
+    # check whether an invader is in front of the space ship
+    def is_invader_ahead(self, row, state):
+        x_pos = state[1]
+        if row == -1:  # end of the grid
+            return False
+        else:
+            if self.rewards[row, x_pos] == 2.0:
+                return True
+            else:
+                return self.is_invader_ahead((row- 1),state)
+
+    # get the correct reward for the step action
+    def reward(self):
         r, c, l = self.current_state
-        if self.shootReward != 0:  # shoot reward
+        if self.shootReward != 0:  # shoot reward, is equal to zero if you take a move action
             return self.shootReward
+        elif self.hit_board_reward != 0: #hit the board boundaries, get "reward penalization"
+            return self.hit_board_reward
         else:  # move reward
             return self.rewards[r, c]
 
     def transition(self, state, a):
-
         action = self.actions[a]
         if a == 2:   # the shoot action has been chosen
-            self.shoot(action['row'])
+            self.shootReward = - 1  # case where you have shot but not invaders have been hit
+            self.shoot(action['row']) # call the shoot function
             return self.current_state
-        else:
+        else:   # move actions
             new_c = state[1] + action[1]
             # check if new position is out of bounds
-            return (state[0], new_c, state[2]) if (new_c >= 0) and (new_c <= self.cols-1) else state
+            if (new_c >= 0) and (new_c <= self.cols - 1):
+                return (state[0], new_c, state[2])
+            else: # reward "penalization" if you hit the boundaries
+                self.hit_board_reward = - 3
+                return state
 
     def step(self, a_idx):
-        self.shootReward = 0   # in every step the actual reward of the shoot action is resetted
+        self.shootReward = 0   # in every step the actual reward of the shoot action & hit board is resetted
+        self.hit_board_reward = 0
 
-        chosen_action = a_idx + np.random.choice([1, 0, -1], p=self._transition_probabilities)   #probability of doing that action
+        chosen_action = a_idx + np.random.choice([1, 0, -1], p=self.transition_probabilities)   # choose the action
         n_actions = 3
-        effective_action = chosen_action + n_actions if chosen_action < 0 else chosen_action % n_actions  # module the index of the action
+        # module the index of the action
+        effective_action = chosen_action + n_actions if chosen_action < 0 else chosen_action % n_actions
 
-        prev_state = self.current_state
+        # do the transition and return all the info of the step
+
+        prev_state = self.getCurrentState()
         self.current_state = self.transition(self.current_state, effective_action)
 
         reward = self.reward()
@@ -115,7 +169,6 @@ class SpaceInvadersEnv(gym.Env):
         info = {"effective_action": effective_action,"sampled_action": a_idx, "prev_state": prev_state}
 
         return observation, reward, done, info
-
 
     def render(self, mode='human'):
         grid = np.array(self.states, dtype=str)
--- HW1 RL/main.py
+++ HW1 RL/main.py
@@ -16,34 +16,35 @@ i->invaders
 6   0   0   0   x   0   0   0
 
 """
-
+import time
+from copy import deepcopy
 import gym
+import numpy as np
 from gym.envs.registration import register
 
-def test(space_env):
+# random execution of the environment with 10 actions
+def random_test(space_env):
 
-    """
     print("************Initial environment***************************\n")
     space_env.render()
-    print(space_env.current_state)
+    print(space_env.getCurrentState())
     print(space_env.reward())
-    print(space_env.termination())
     print(space_env.action_semantics)
-    """
-
-    print("\n***********5 random actions********************************\n")
 
 
-    for i in range(1000):
+    print("\n***********10 random actions********************************\n")
+
+
+    for i in range(10):
         # print the transaction
         print("\n**********************"+ " iteration number: "+ str(i+1) + "*************************************\n" )
         observation, reward, done, info =space_env.step(space_env.action_space.sample())
         print("observation: " + str(observation))
         print("reward: " +str(reward))
         print("game complete: " + str(done))
-        print("sampled action: " + env.action_semantics[info["sampled_action"]])
-        print("effective action: " + env.action_semantics[info["effective_action"]])
-        print("prev state: " + str(info["prev_state"]))
+        print("sampled action: " + space_env.action_semantics[info["sampled_action"]])
+        print("effective action: " + space_env.action_semantics[info["effective_action"]])
+        print("has invader/s ahead: " + str(space_env.is_invader_ahead(4, observation)))
         space_env.render()
 
         if done:
@@ -52,26 +53,178 @@ def test(space_env):
     space_env.reset()
 
 
+# function which computes the Value function
+def policyEvaluation(pi, V, env, theta=0.1, discount = 0.8):
+    state_space_size = env.cols # * 2
+    while True:
+        delta = 0 # variable used to track the updates
+        for s in range(state_space_size):
+            # initialization of v to zero then updated
+            v = 0
+            for act,action_prob in enumerate(pi[s]):
+                # get the actual state of the environment (invaders disposition) and place the space ship in s
+                env.setCurrentState((env.getCurrentState()[0], s, env.getCurrentState()[2]))
+
+                # do the step and get results
+                observation, reward, done, info = env.step(act)
+                transition_prob = env.transition_probabilities[1]  # the chosen action index + 0
+
+                # update v
+                v += action_prob * transition_prob * (reward + discount* (V[observation[1]]))
+            # take the max variation
+            delta = max(delta, np.abs(v - V[s]))
+            # update for the state
+            V[s]= v
+        # condition to leave the loop
+        if delta < theta:
+            break
+    return V
+
+
+def policyIteration(env, discount = 0.8):
+    # retrieve state space and actions for the environment
+    state_space_size = env.cols
+    actions = env.action_semantics
+
+    # vector of utilities per states in S initially zero
+    V = np.zeros(state_space_size)
+
+    # initialization of the policy, with an equal probability for all actions in states
+    pi = np.ones([state_space_size, len(actions)]) / len(actions)
+    # counting the number of step. useful for debugging
+    step = 0
+    # main loop
+    while True:
+        # update the step
+        step += 1
+        # compute the Value function
+        V = policyEvaluation(pi, V, env)
+        # get the transition probability
+        transition_prob = env.transition_probabilities[1]
+        # Variable to check if there's an update of the choosen action for the state,setted to false if not
+        policy_stable = True
+        for s in range(state_space_size):
+
+            # choose the best action fot the current policy
+            old_action = np.argmax(pi[s])
+            # definition of an array for the policy values
+            acts_values = np.zeros(len(actions))
+            for a in range (len(actions)):
+                # set up of the environment
+                env.reset()
+                env.setCurrentState((env.getCurrentState()[0], s, env.getCurrentState()[2]))
+
+                # do the step and get results
+                observation, reward, done, info = env.step(a)
+                # compute the policy values
+                acts_values[a] += transition_prob * (reward + discount * V[observation[1]])
+
+            # choose the new action as the best among them
+            new_action = np.argmax(acts_values)
+
+            # check for updates in the actions
+            if old_action != new_action:
+                policy_stable = False
+            # update the policy for this state
+            pi[s]=  np.eye(len(actions))[new_action] # acts_values
+
+            # if you complete the game go to the next state evaluation
+            if done:
+                break
+        # if no update, return the policy
+        if policy_stable:
+            env.reset()
+            return pi
+
+
+
+# ************************** Booting *******************************
+
+# function to give a look for the results of the policy evaluation
+def testPolicyEvaluation(env):
+    state_space_size = env.cols
+    actions = env.action_semantics
+    V = np.zeros(state_space_size)
+    pi = np.ones([state_space_size, len(actions)]) / len(actions)
+    V = policyEvaluation(pi, V, env)
+    print(V)
+# function to give a look for the results of the policy iteration
+def testPolicyIteration(env):
+    pi, V = policyIteration(env)
+    print("Policy: " + str(pi))
+
+# function which test the policy
+def policyTest(env, testType = "on_environment"):
+    # reset the environment before test
+    env.reset()
+    if testType == "policy_eval":
+        testPolicyEvaluation(env)
+    elif testType == "policy_iter":
+        testPolicyIteration(env)
+    # test on_your environment
+    else:
+        # to compute the policy has been done this reasoning:
+        # take a reduce space state (only thinking about the space ship possible positions, hence 7 states,
+        # instead of 2^35 * 7 possible states considering all the possible invaders permutations. A new policy will be
+        # compute for each loop passing the actual state with the configuration of invaders.
+        # brief consideration: the procedure take enough time for each step, the results are not allways good, the
+        # choosen action by the policy sometimes is improper, i wrote the algorithm following the instruction on the
+        # book "RLbook2018".
+
+        for i in range(5):
+            # make an image of the environment before the policy evaluation
+            state = deepcopy(env.current_state)
+            reward = deepcopy(env.rewards)
+
+            # policy calculus
+            policy = policyIteration(env)
+            time.sleep(1)
+            print("****Policy****:\n")
+            print(policy)
+
+            # restore the image of the environment
+            env.setCurrentState(state)
+            env.setRewards(reward)
+
+            # act following the policy, print section
+            print("\n**********************" + " iteration number: " + str(i + 1) + "*************************************\n")
+            x_pos = env.getCurrentState()[1]
+            action = np.argmax(policy[x_pos])
+            observation, reward, done, info = env.step(action)
+            print("observation: " + str(observation))
+            print("reward: " + str(reward))
+            print("game complete: " + str(done))
+            print("choosen action: " + env.action_semantics[info["sampled_action"]])
+            print("effective action: " + env.action_semantics[info["effective_action"]])
+            print("has invader/s ahead: " + str(env.is_invader_ahead(4, observation)))
+            env.render()
+
+            # if complete exit from the loop
+            if done:
+                break
 
 if __name__ == "__main__":
     register(
         id='spaceInvaders-v0',
         entry_point='gym_game.env:SpaceInvadersEnv',
-        max_episode_steps=2000,
+        max_episode_steps=10000,
     )
+    # creation of the gum environment
     env = gym.make('spaceInvaders-v0')
     env.reset()
 
-    MAX_EPISODES = 9999
-    MAX_TRY = 1000
-    epsilon = 1
-    epsilon_decay = 0.999
-    learning_rate = 0.1
-    gamma = 0.6
-
+    print("*************************************************************************************")
+    print("********************************* Random test ***************************************")
+    print("*************************************************************************************")
     # first task of the homework
-    test(env)
+    random_test(env)
 
+
+    print("*************************************************************************************")
+    print("********************************* Policy test ***************************************")
+    print("*************************************************************************************")
     # second task of the homework
+    policyTest(env,testType= "on_environment")
+
 
 
